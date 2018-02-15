8f2153ec2a15ba522dcd9b512d9882d2f71a079e
likwokbiu <li_bill@ymail.com>
2018-02-15 17:20:53 -0500

    For Part 3

--- crm.rb
+++ crm.rb
@@ -6,18 +6,35 @@ get '/' do
 end
 
 get '/home' do
-  erb(:index)
+  @title = 'Home'
+  erb :index
+end
+
+get '/add' do
+  @title = 'Add'
+  erb :add
 end
 
 get '/contacts' do
-  @title = 'Contact List'
-  @currect_time = Time.now
-  @contact_list = Contact.all
-  erb(:contacts)
+  @title = 'Contacts'
+  @contacts_list = Contact.all
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  @title = 'Contact Informations'
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    @title = 'Not found'
+    erb :not_found
+  end
 end
 
 get '/about' do
-  erb(:about)
+  @title = 'About'
+  erb(:About)
 end
 
 at_exit do
