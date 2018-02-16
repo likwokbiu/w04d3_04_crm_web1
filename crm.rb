a9f4903c3eda024b37f3af729ace149b419c1ad1
likwokbiu <li_bill@ymail.com>
2018-02-16 17:11:44 -0500

    Part 4

--- crm.rb
+++ crm.rb
@@ -10,9 +10,19 @@ get '/home' do
   erb :index
 end
 
-get '/add' do
-  @title = 'Add'
-  erb :add
+get '/contacts/new' do
+  @title = 'New'
+  erb :new
+end
+
+post '/contacts' do
+  Contact.create(
+  first_name: params[:first_name],
+  last_name:  params[:last_name],
+  email:      params[:email],
+  note:       params[:note]
+  )
+  redirect to('/contacts')
 end
 
 get '/contacts' do
@@ -29,6 +39,42 @@ get '/contacts/:id' do
   else
     @title = 'Not found'
     erb :not_found
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    @title = 'Not found'
+    erb :not_found
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+    redirect to('/contacts')
+  else
+    @title = 'Not found'
+    erb :not_found
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
   end
 end
 
